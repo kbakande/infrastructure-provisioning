efd994a796a5faab47cd0f3c18f36e09eea3a023
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2023-11-12 12:07:06 +0000

    add variable type

---
+++ modules/project/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+    type = string
+    description = "The ID of the Google Cloud project"
+}
+
+variable "project_name" {
+    type = string
+    description = "The name of the Google Cloud project"
+}
+
+variable "org_id" {
+    type = string
+    description = "The ID of the Google Cloud organization"
+}
+
+# Add more variables as needed

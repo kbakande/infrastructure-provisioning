d48960af4aecd13c3284153ced4ca513072119c8
Kabeer Akande <46060542+kbakande@users.noreply.github.com>
2023-11-13 19:01:38 +0000

    adds variable script

---
+++ modules/bigquery_datasets/variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+}
+
+variable "dataset_id" {
+  description = "The ID of the BigQuery dataset"
+}
+
+variable "dataset_location" {
+  description = "The location of the BigQuery dataset"
+}
+
+variable "default_table_expiration_ms" {
+  description = "The default table expiration time in milliseconds"
+}
+
+# Add more variables as needed
